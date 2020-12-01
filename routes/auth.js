16ca5a8cc5e938eec324da3b75337e0a4c2e6ded
cardfaux <cardfaux@tutanota.com>
2020-12-01 13:41:38 -0600

    got user signup sigin signout and protected routes with validation set up on the backend, tested routes with postman moving to the nextjs frontend

--- routes/auth.js
+++ routes/auth.js
@@ -9,11 +9,11 @@ const { userSignupValidator, userSigninValidator } = require('../validators/auth
 router.post('/signup', userSignupValidator, runValidation, signup);
 router.post('/signin', userSigninValidator, runValidation, signin);
 router.get('/signout', signout);
-// test
-// router.get('/secret', requireSignin, (req, res) => {
-//     res.json({
-//         user: req.user
-//     });
-// });
+//test
+router.get('/secret', requireSignin, (req, res) => {
+  res.json({
+    message: 'you have access to secret page'
+  });
+});
 
 module.exports = router;
