60e6740a540acd8248a32f40e5bb3b44c1e14b26
cardfaux <cardfaux@tutanota.com>
2020-12-01 11:27:42 -0600

    first commit

---
+++ server.js
@@ -0,0 +1,45 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const chalk = require('chalk');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
+// BRING IN ROUTES
+const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
+
+// APP
+const app = express();
+
+
+// DATABASE CONNECTION
+mongoose
+  .connect(process.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  })
+  .then(() => console.log(chalk.bgMagentaBright.cyanBright('DATABASE CONNECTED')));
+
+// MIDDLEWARES
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+if (process.env.NODE_ENV === 'development') {
+  app.use(cors({ origin: `${process.env.CLIENT_URL}` }));
+}
+// ROUTES MIDDLEWARES
+app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
+
+// PORT
+const port = process.env.PORT || 8000;
+
+// START SERVER
+app.listen(port, () => {
+  console.log(chalk.bgCyanBright.magentaBright(`Server Is Running On Port ${ port }`));
+})
