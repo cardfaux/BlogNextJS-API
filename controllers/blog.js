7c035bea6799cbdb53f263f5fe053bd38fa11eee
cardfaux <cardfaux@tutanota.com>
2020-12-06 12:20:35 -0600

    added the .result to the mdesc field in the database

--- controllers/blog.js
+++ controllers/blog.js
@@ -51,7 +51,8 @@ exports.create = (req, res) => {
     blog.excerpt = smartTrim(body, 320, ' ', ' ...');
     blog.slug = slugify(title).toLowerCase();
     blog.mtitle = `${title} | ${process.env.APP_NAME}`;
-    blog.mdesc = stripHtml(body.substring(0, 160));
+    // blog.mdesc = stripHtml(body.substring(0, 160));
+    blog.mdesc = stripHtml(body.substring(0, 160)).result;
     //blog.postedBy = req.user._id;
     blog.postedBy = req.auth._id;
     // categories and tags
