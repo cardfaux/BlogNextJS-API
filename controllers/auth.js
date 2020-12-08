be8e56eda5c909f9bfd12fca3b3a052a3a3adfe1
cardfaux <cardfaux@tutanota.com>
2020-12-08 12:13:26 -0600

    removed the old signup controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -42,32 +42,6 @@ exports.preSignup = (req, res) => {
   });
 };
 
-// exports.signup = (req, res) => {
-//   User.findOne({ email: req.body.email }).exec((err, user) => {
-//     if (user) {
-//       return res.status(400).json({
-//         error: 'Email is taken'
-//       });
-//     }
-
-//     const { name, email, password } = req.body;
-//     let username = nanoid();
-//     let profile = `${process.env.CLIENT_URL}/profile/${username}`;
-
-//     let newUser = new User({ name, email, password, profile, username });
-//     newUser.save((err, success) => {
-//       if (err) {
-//         return res.status(400).json({
-//           error: err
-//         });
-//       }
-//       res.json({
-//         message: 'Signup success! Please signin.'
-//       });
-//     });
-//   });
-// };
-
 exports.signup = (req, res) => {
   const token = req.body.token;
   if (token) {
