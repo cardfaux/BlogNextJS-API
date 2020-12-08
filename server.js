d6f212a7c84c304f5c026bfa5fb4d75a1ddd79f0
cardfaux <cardfaux@tutanota.com>
2020-12-07 18:11:07 -0600

    added the routes for contacting sendgrid

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/user');
 const categoryRoutes = require('./routes/category');
 const tagRoutes = require('./routes/tag');
+const formRoutes = require('./routes/form');
 
 // APP
 const app = express();
@@ -44,6 +45,7 @@ app.use('/api', authRoutes);
 app.use('/api', userRoutes);
 app.use('/api', categoryRoutes);
 app.use('/api', tagRoutes);
+app.use('/api', formRoutes);
 
 // PORT
 const port = process.env.PORT || 8000;
